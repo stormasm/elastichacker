85347b4b7cda2af9bace67f5f8e3540019f5bd96
Michael Angerman <stormasm@gmail.com>
2017-04-28 11:20:49 -0700

    switch from passing the parameters in on the command line to creating a method call

--- cmd/bulkredis.go
+++ cmd/bulkredis.go
@@ -3,42 +3,20 @@ package main
 import (
 	"context"
 	"errors"
-	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	//"strconv"
-	"time"
 
 	"github.com/olivere/elastic"
 	"github.com/stormasm/elastichacker/redisc"
 	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	var (
-		index    = flag.String("index", "", "Elasticsearch index name")
-		typ      = flag.String("type", "", "Elasticsearch type name")
-		n        = flag.Int("n", 0, "Number of documents to bulk insert")
-		bulkSize = flag.Int("bulk-size", 0, "Number of documents to collect before committing")
-	)
-	flag.Parse()
-	log.SetFlags(0)
-	rand.Seed(time.Now().UnixNano())
+// index    Elasticsearch index name
+// typ      Elasticsearch type name
+// bulkSize Number of documents to collect before committing
 
-	if *index == "" {
-		log.Fatal("missing index parameter")
-	}
-	if *typ == "" {
-		log.Fatal("missing type parameter")
-	}
-	if *n <= 0 {
-		log.Fatal("n must be a positive number")
-	}
-	if *bulkSize <= 0 {
-		log.Fatal("bulk-size must be a positive number")
-	}
+func gorun(index string, typ string, bulkSize int) {
 
 	// Do a trace log
 	tracelog := log.New(os.Stdout, "", 0)
@@ -63,6 +41,8 @@ func main() {
 	g.Go(func() error {
 		defer close(docsc)
 
+		// Eventually one could pass in an array of strings which
+		// would be the keys one can pull from redis...
 		go redisc.Hscan("story", docsc)
 		go redisc.Hscan("comment", docsc)
 
@@ -75,7 +55,7 @@ func main() {
 
 	// Second goroutine will consume the documents sent from the first and bulk insert into ES
 	g.Go(func() error {
-		bulk := client.Bulk().Index(*index).Type(*typ)
+		bulk := client.Bulk().Index(index).Type(typ)
 
 		for d := range docsc {
 			// Enqueue the document
@@ -83,7 +63,7 @@ func main() {
 			mydoc := d.Json
 			bulk.Add(elastic.NewBulkStringRequest().Id(countstr).SetSource(mydoc))
 
-			if bulk.NumberOfActions() >= *bulkSize {
+			if bulk.NumberOfActions() >= bulkSize {
 				// Commit
 				res, err := bulk.Do(ctx)
 				if err != nil {
@@ -117,4 +97,8 @@ func main() {
 	if err := g.Wait(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	gorun("warehouse", "product", 3)
 }
